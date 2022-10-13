04b80996c1760af9aadbd39f40a4fac21c9d17e8
killian31 <80256683+killian31@users.noreply.github.com>
2022-10-13 15:05:57 +0200

    trying different colors

--- draw.py
+++ draw.py
@@ -3,7 +3,12 @@ import random
 import numpy as np
 from tqdm import tqdm
 
-def y(level, angle, pbar, sz=100):
+
+def initialize_color(level):
+    if level > 1:
+        pencolor(random.randint(0, 256)//level, random.randint(0, 256)//level, random.randint(0, 256)//level)
+
+def y(level, angle, pbar, sz=150):
     pbar.update(1)
     if level > 0:
         colormode(255)
@@ -13,7 +18,7 @@ def y(level, angle, pbar, sz=100):
         # setting the colour according
         # to the current level
         pencolor(0, 255//level, 0)
-        
+        initialize_color(level)
         # drawing the base
         fd(sz)
 
@@ -24,7 +29,7 @@ def y(level, angle, pbar, sz=100):
         y(level = level - 1, angle = angle, pbar=pbar, sz = 0.8 * sz)
         
         pencolor(0, 255//level, 0)
-        
+        initialize_color(level)
         lt( 2 * angle )
 
         # recursive call for
@@ -68,6 +73,9 @@ def main():
     print(f"\nDrawing with {lvl} levels of depth.\n")
     pbar = tqdm(range(1, lines+1))
     hideturtle()
+    pensize(width=3)
+    colormode(255)
+    pencolor(random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
     screen = Screen()
     screen.setup(width = 1.0, height = 1.0)
     speed(0.8)
@@ -78,7 +86,7 @@ def main():
     pendown()
     rt(-180)
     # the acute angle between the base and branch of the Y
-    angle = random.randint(20, 65)
+    angle = random.randint(15, 65)
     y(level = lvl, angle = angle, pbar = pbar)
     
     done()
